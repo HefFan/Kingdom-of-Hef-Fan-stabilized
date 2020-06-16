b966c993daca56eb56f0bbc2da84bc982b710df8
Hef Fan Kingdom <heffankingdom@gmail.com>
2020-06-16 19:35:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+document.getElementById("_unmenu").style.visibility="hidden";
+document.getElementById("menu").style.visibility="hidden";
+document.getElementById("confirm_").style.visibility="visible";
+
+function mob(){
+	var l= document.getElementById("css").href="mobile.css"
+	}
+
+function deks(){
+	var l= document.getElementById("css").href="style.css"
+	}
+
+
+function menu() {
+
+	document.getElementById("menu").style.visibility = "visible";
+	document.getElementById("_unmenu").style.visibility = "visible";
+	document.getElementById("_menu").style.visibility = "hidden";
+	
+	}
+
+
+	function unmenu() {
+
+	document.getElementById("menu").style.visibility = "hidden";
+	document.getElementById("_unmenu").style.visibility = "hidden";
+	document.getElementById("_menu").style.visibility = "visible";
+	}
+
+function out(){
+	document.getElementById("menu").style.visibility="hidden";
+	}
+
+function over(){
+	document.getElementById("menu").style.visibility="visible";
+	}
+
+function showid(){
+	alert("Line Official Account [Add friend by id] \n Id: @388kkaww ");
+	}
+
+function hidthis(){
+	document.getElementById("confirm_").style.visibility="hidden";
+	}
