1019dbc7125aec991eb86cfb43d3886c7e457d57
Hef Fan Kingdom <heffankingdom@gmail.com>
2020-06-17 18:59:20 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-	<table>
-	<thead> <td>Username</td> <td>Email</td> <td>Password</td> </thead>
-	<tr> <td><?php echo $_POST["username"]; ?></td> <td><?php echo $_POST["email"]; ?></td> <td><?php $_POST["psw"]; ?></td>
-	</table>
-</body>
-</html>
