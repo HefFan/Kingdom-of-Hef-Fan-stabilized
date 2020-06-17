ea82bd0289afb5d75eddca5ab43c75cae2020f6f
Hef Fan Kingdom <heffankingdom@gmail.com>
2020-06-17 13:48:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
 	<table>
 	<thead> <td>Username</td> <td>Email</td> <td>Password</td> </thead>
-	<tr> <td><?php echo $_POST["username"]; ?></td> <td><?php echo $_POST["email"]; ?></td> <td><?php $_POST["psw"]; ?></td>
+	<tr> <td><?php echo $_GET["username"]; ?></td> <td><?php echo $_GET["email"]; ?></td> <td><?php $_GET["psw"]; ?></td>
 	</table>
 </body>
 </html>
